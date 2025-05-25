7d104a603e5689c465d20c58e8b39dcb329d8726
dennisjs <dennisj@outlook.com>
2025-05-25 09:47:56 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 
 window.CONFIG = {
   MAPBOX_TOKEN: "pk.eyJ1IjoiZGVubmlzanMiLCJhIjoiY21iM3RsbDBkMHlkeTJpcTNqdXgzbmJ6dSJ9.Wm3ylbaIO2j5TUorrNTdmg",
-  OPENWEATHER_KEY: "0f8e3622808ddddedef556c32d470ffa"
+  OPENWEATHER_KEY: "https://silent-snow-b58a.dennisj.workers.dev"
 };
 
