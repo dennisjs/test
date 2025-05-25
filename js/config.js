637e4aee3c49c407481ef949728d3ec35032ca73
dennisjs <dennisj@outlook.com>
2025-05-25 08:37:16 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 
 window.CONFIG = {
-  MAPBOX_TOKEN: "pk.eyJ1IjoiZGVubmlzanMiLCJhIjoiY21iM3FoMmdoMHk5MjJtb2ltZjFwbnpoaiJ9.emvdyGR-WKAwS3pPbCXbBQ",
+  MAPBOX_TOKEN: "pk.eyJ1IjoiZGVubmlzanMiLCJhIjoiY21iM3RsbDBkMHlkeTJpcTNqdXgzbmJ6dSJ9.Wm3ylbaIO2j5TUorrNTdmg",
   OPENWEATHER_KEY: "0f8e3622808ddddedef556c32d470ffa"
 };
 
